3a65d50048462c202db962e5e0d6f45a935b7e29
Joshua Bisdorf <joshua@bisdorf.org>
2026-09-12 15:11:26 -0400

    Make startup migrations explicit for Kubernetes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,17 @@
 set -eu
 
 python -m app.policy_cli validate
-python -m app.database migrate
+
+case "${SAG_RUN_MIGRATIONS:-true}" in
+  true|1|yes|on)
+    python -m app.database migrate
+    ;;
+  false|0|no|off)
+    ;;
+  *)
+    echo "ERROR startup reason=invalid_SAG_RUN_MIGRATIONS" >&2
+    exit 2
+    ;;
+esac
 
 exec "$@"
