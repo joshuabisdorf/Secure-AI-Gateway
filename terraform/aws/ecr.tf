16b21280713c663fc9a300c0e4bda02cd0c41224
Joshua Bisdorf <joshua@bisdorf.org>
2026-09-12 15:36:05 -0400

    Provision encrypted immutable ECR repository

---
+++ terraform/aws/ecr.tf
@@ -0,0 +1,49 @@
+resource "aws_ecr_repository" "gateway" {
+  name                 = local.name
+  image_tag_mutability = "IMMUTABLE"
+  force_delete         = var.ecr_force_delete
+
+  encryption_configuration {
+    encryption_type = "KMS"
+    kms_key         = aws_kms_key.platform.arn
+  }
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "gateway" {
+  repository = aws_ecr_repository.gateway.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire untagged images after 14 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 14
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
+        description  = "Retain the newest 50 tagged images"
+        selection = {
+          tagStatus   = "tagged"
+          tagPrefixList = ["v", "sha-"]
+          countType   = "imageCountMoreThan"
+          countNumber = 50
+        }
+        action = {
+          type = "expire"
+        }
+      },
+    ]
+  })
+}
